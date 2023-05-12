d4294c484881814630691eddd50ec29be37b730a
Felipidis <felipekdra@gmail.com>
2023-05-11 22:56:41 -0300

    feat: add auth module to app

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,17 @@
 import { Module } from '@nestjs/common';
 import { AttendanceModule } from './Modules/attendance/attendance.module';
+import { AuthModule } from './Modules/auth/auth.module';
 import { ClientModule } from './Modules/client/client.module';
 import { JobModule } from './Modules/job/job.module';
 import { ProfessionalModule } from './Modules/professional/professional.module';
 
 @Module({
-  imports: [ClientModule, ProfessionalModule, JobModule, AttendanceModule],
+  imports: [
+    ClientModule,
+    ProfessionalModule,
+    JobModule,
+    AttendanceModule,
+    AuthModule,
+  ],
 })
 export class AppModule {}
