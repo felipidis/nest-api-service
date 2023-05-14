115dd80ce1eaa9330b403374684083bc31f1582e
Felipe Pimentel <felipekdra@gmail.com>
2023-05-14 12:13:55 -0300

    chore: removing comments

--- src/prisma-client-exception/prisma-client-exception.filter.ts
+++ src/prisma-client-exception/prisma-client-exception.filter.ts
@@ -21,7 +21,6 @@ export class PrismaClientExceptionFilter extends BaseExceptionFilter {
         break;
       }
       default:
-        // default 500 error code
         super.catch(exception, host);
         break;
     }
